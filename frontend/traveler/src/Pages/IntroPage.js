509a14510066a9a415e4cb52402aa2e63ae099c5
Tammas Hicks <50436881+ScriptkidHicks@users.noreply.github.com>
2021-10-04 20:51:30 -0700

    landing page title

--- frontend/traveler/src/Pages/IntroPage.js
+++ frontend/traveler/src/Pages/IntroPage.js
@@ -3,6 +3,9 @@ import classes from "../CSS/IntroPage.module.css";
 function IntroPage() {
   return (
     <div className={classes.mainbody}>
+      <h1 className={classes.introTitle}>
+        Welcome to Traveler<sup className={classes.trademark}>tm</sup>
+      </h1>
       <a
         href="https://www.youtube.com/watch?v=JB6AEy3PCdw&ab_channel=LearnDesign"
         className={classes.shineBox}
