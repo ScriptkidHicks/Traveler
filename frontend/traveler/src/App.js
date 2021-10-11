0c49874f2a1998d7a93dd7c15e96734a3adf5d8c
Jordan Smith <jssmith9876@yahoo.com>
2021-10-10 17:02:44 -0700

    merge

--- frontend/traveler/src/App.js
+++ frontend/traveler/src/App.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-=======
-import { Route, Switch } from "react-router-dom";
-import "./CSS/App.css";
-
-import IntroPage from "./Pages/IntroPage";
-import MainPage from "./Pages/MainPage";
->>>>>>> DevTammas
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
@@ -20,7 +12,6 @@ function App() {
   }, []);
 
   return (
-<<<<<<< HEAD
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -37,17 +28,6 @@ function App() {
         </a>
         <p>The current time is {currentTime}.</p>
       </header>
-=======
-    <div>
-      <Switch>
-        <Route path="/" exact>
-          <IntroPage />
-        </Route>
-        <Route path="/MainPage">
-          <MainPage />
-        </Route>
-      </Switch>
->>>>>>> DevTammas
     </div>
   );
 }
