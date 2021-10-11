2e4d37cf764006b11f043be6d9fb650832fe413c
Jordan Smith <jssmith9876@yahoo.com>
2021-10-11 10:19:51 -0700

    Post request has been successfully recieved on the backend

--- server/app.py
+++ server/app.py
@@ -5,12 +5,7 @@ app = flask.Flask(__name__)
 
 @app.route("/get_order", methods=["POST"])
 def get_order():
-    locations = json.loads(flask.request.form.get('locations'))
+    locations = flask.request.data
     print(locations)
 
-
     return locations
-
-# @app.route("/time")
-# def get_time():
-#     return {'time': time.strftime("%H:%M", time.localtime())}
