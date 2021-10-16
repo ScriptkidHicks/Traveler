25d81e183bcf0877cc20af6f942c05c1339d7fb8
Tammas Hicks <50436881+ScriptkidHicks@users.noreply.github.com>
2021-10-16 07:36:02 -0700

    Working on implementing multiple inputs

--- frontend/traveler/src/Pages/MainPage.js
+++ frontend/traveler/src/Pages/MainPage.js
@@ -1,11 +1,39 @@
 import GOOGLEMAPS_API_KEY from "../key";
 
+import styled from "styled-components";
+
 import classes from "../CSS/MainPage.module.css";
 import React, { useState } from "react";
 import AutocompleteComponent from "../Components/AutocompleteComponent";
 
 function MainPage() {
+  // this method of doing this is a little uhhhhhhhhhhhhhh bad, but it works
+  // I can revisit it in the future. Maybe a struct that can be imported.
   const [placeOne, setPlaceOne] = useState(null);
+  const [placeTwo, setPlaceTwo] = useState(null);
+  const [placeThree, setPlaceThree] = useState(null);
+  const [placeFour, setPlaceFour] = useState(null);
+  const [placeFive, setPlaceFive] = useState(null);
+  const [placeSix, setPlaceSix] = useState(null);
+  const [placeSeven, setPlaceSeven] = useState(null);
+  const [placeEight, setPlaceEight] = useState(null);
+  const [placeNine, setPlaceNine] = useState(null);
+  const [placeTen, setPlaceTen] = useState(null);
+  const [origin, setOrigin] = useState(null);
+
+  const places = [
+    origin,
+    placeOne,
+    placeTwo,
+    placeThree,
+    placeFour,
+    placeFive,
+    placeSix,
+    placeSeven,
+    placeEight,
+    placeNine,
+    placeTen,
+  ];
 
   function postToBackend() {
     const requestOptions = {
@@ -14,7 +42,7 @@ function MainPage() {
         "Content-Type": "application/json",
         Contents: "addresses",
       },
-      body: JSON.stringify({ title: "Set of locations", locations: placeOne }),
+      body: JSON.stringify({ title: "Set of locations", locations: places }),
     };
 
     fetch("/get_order", requestOptions)
@@ -25,10 +53,31 @@ function MainPage() {
 
   return (
     <div className={classes.mainBody}>
-      <AutocompleteComponent placeChanger={setPlaceOne} />
-      <button onClick={postToBackend}>Click Me</button>
+      <InputWrapper>
+        <InputLabel>Place of Origin</InputLabel>
+        <AutocompleteComponent placeChanger={setOrigin} />
+        <AutocompleteComponent placeChanger={setPlaceOne} />
+        <AutocompleteComponent placeChanger={setPlaceTwo} />
+        <AutocompleteComponent placeChanger={setPlaceThree} />
+        <AutocompleteComponent placeChanger={setPlaceFour} />
+        <AutocompleteComponent placeChanger={setPlaceFive} />
+        <AutocompleteComponent placeChanger={setPlaceSix} />
+        <AutocompleteComponent placeChanger={setPlaceSeven} />
+        <AutocompleteComponent placeChanger={setPlaceEight} />
+        <AutocompleteComponent placeChanger={setPlaceNine} />
+        <AutocompleteComponent placeChanger={setPlaceTen} />
+        <button onClick={postToBackend}>Click Me</button>
+      </InputWrapper>
     </div>
   );
 }
 
 export default MainPage;
+
+const InputLabel = styled.label`
+  color: green;
+`;
+
+const InputWrapper = styled.div`
+  background-color: black;
+`;
