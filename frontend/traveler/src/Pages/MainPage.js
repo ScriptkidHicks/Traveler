0b7f5917de10fbaa84f913afa48304dcce37834d
Tammas Hicks <50436881+ScriptkidHicks@users.noreply.github.com>
2021-10-29 14:49:54 -0700

    font size fixes

--- frontend/traveler/src/Pages/MainPage.js
+++ frontend/traveler/src/Pages/MainPage.js
@@ -13,59 +13,55 @@ function MainPage(props) {
   return (
     <div className={classes.mainBody}>
       <div className={classes.container}>
-        <div className={classes.formWrapper}>
-          <form className={classes.form}>
-            <h1 className={classes.formTitle}>Destinations</h1>
-            <AutocompleteComponent
-              placeChanger={props.setOrigin}
-              placeholder={"Place of origin"}
-            />
-            <AutocompleteComponent
-              placeChanger={props.setOne}
-              placeholder={"Location one"}
-            />
-            <AutocompleteComponent
-              placeChanger={props.setTwo}
-              placeholder={"Location two"}
-            />
-            <AutocompleteComponent
-              placeChanger={props.setThree}
-              placeholder={"Location three"}
-            />
-            <AutocompleteComponent
-              placeChanger={props.setFour}
-              placeholder={"Location four"}
-            />
-            <AutocompleteComponent
-              placeChanger={props.setFive}
-              placeholder={"Location five"}
-            />
-            <AutocompleteComponent
-              placeChanger={props.setSix}
-              placeholder={"Location six"}
-            />
-            <AutocompleteComponent
-              placeChanger={props.setSeven}
-              placeholder={"Location seven"}
-            />
-            <AutocompleteComponent
-              placeChanger={props.setEight}
-              placeholder={"Location eight"}
-            />
-            <AutocompleteComponent
-              placeChanger={props.setNine}
-              placeholder={"Location nine"}
-            />
-            <AutocompleteComponent
-              placeChanger={props.setTen}
-              placeholder={"Location ten"}
-            />
-          </form>
-          <Link className={classes.form__button} to="/Results">
-            Submit Locations
-          </Link>
-        </div>
+        <h1 className={classes.formTitle}>Destinations</h1>
+        <AutocompleteComponent
+          placeChanger={props.setOrigin}
+          placeholder={"Place of origin"}
+        />
+        <AutocompleteComponent
+          placeChanger={props.setOne}
+          placeholder={"Location one"}
+        />
+        <AutocompleteComponent
+          placeChanger={props.setTwo}
+          placeholder={"Location two"}
+        />
+        <AutocompleteComponent
+          placeChanger={props.setThree}
+          placeholder={"Location three"}
+        />
+        <AutocompleteComponent
+          placeChanger={props.setFour}
+          placeholder={"Location four"}
+        />
+        <AutocompleteComponent
+          placeChanger={props.setFive}
+          placeholder={"Location five"}
+        />
+        <AutocompleteComponent
+          placeChanger={props.setSix}
+          placeholder={"Location six"}
+        />
+        <AutocompleteComponent
+          placeChanger={props.setSeven}
+          placeholder={"Location seven"}
+        />
+        <AutocompleteComponent
+          placeChanger={props.setEight}
+          placeholder={"Location eight"}
+        />
+        <AutocompleteComponent
+          placeChanger={props.setNine}
+          placeholder={"Location nine"}
+        />
+        <AutocompleteComponent
+          placeChanger={props.setTen}
+          placeholder={"Location ten"}
+        />
       </div>
+      <Link className={classes.form__button} to="/Results">
+        Submit Locations
+      </Link>
     </div>
   );
 }
